0489a9e680e30363e4eb94eab9c0af347f892968
Gaurav232003 <gauravagarwal232003>
2023-09-13 08:13:23 +0530

    lol

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hackheads/components/data.dart';
 import 'login.dart';
+import 'package:hackheads/components/data.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,16 +23,14 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const Login(),
-    );
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+    print('height: ');
+    print(height);
+    print('w');
+    print(width);
+    return const Login();
   }
 }
